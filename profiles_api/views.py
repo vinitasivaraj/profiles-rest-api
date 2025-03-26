eb82b93fba37dacae994ca26b72c7cd928026936
Vinitha sivaraj <vinitasivaraj.1995@gmail.com>
2025-03-26 19:11:50 +0530

    created helooapiview

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelooApi(APIView):
+
+    def get(self,request,format=None):
+        an_apiview=[
+            "uses Http method",'is similar to django view','is mapped manually',
+        ]
+        return Response({'message':"Hello",'an_apiview':an_apiview})
